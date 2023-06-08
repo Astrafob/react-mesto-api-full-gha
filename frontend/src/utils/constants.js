1a29c0217491b52a454684053dc5bd5ac393c89a
Oleg Vinogradov <astrafob@yandex.ru>
2023-06-08 21:52:09 +0300

    add cors

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,17 +1,17 @@
 export const apiConfig = {
-  // url: 'http://localhost:3000',
+  url: 'http://localhost:3000',
   // url: 'https://api.mesto.astra.nomoredomains.rocks',
-  url: 'https://mesto.nomoreparties.co/v1/cohort-60',
+  // url: 'https://mesto.nomoreparties.co/v1/cohort-60',
   headers: {
-    authorization: 'c9936e4c-070a-4b7d-9c19-b49f7cce0362',
-    // 'authorization': `Bearer ${localStorage.getItem('jwt')}`,
+    // authorization: 'c9936e4c-070a-4b7d-9c19-b49f7cce0362',
+    'authorization': `Bearer ${localStorage.getItem('jwt')}`,
     'Content-Type': 'application/json'
   }
 };
 
 export const authApiConfig = {
-  url: 'https://auth.nomoreparties.co',
-  // url: 'http://localhost:3000',
+  // url: 'https://auth.nomoreparties.co',
+  url: 'http://localhost:3000',
   // url: 'https://api.mesto.astra.nomoredomains.rocks',
   headers: {
     'Content-Type': 'application/json'
